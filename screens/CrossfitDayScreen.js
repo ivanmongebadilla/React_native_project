d9c1ef69b792aa1b3242d04958f663aa7f17bb6c
Ivan Monge <ivanmongebadilla@gmail.com>
2023-02-04 08:52:57 -0800

    CrossfitDayScreen style

--- screens/CrossfitDayScreen.js
+++ screens/CrossfitDayScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, FlatList, StyleSheet, Modal, TouchableOpacity } from "react-native";
+import { View, Text, FlatList, StyleSheet, Modal, TouchableOpacity, Button } from "react-native";
 import { WebView } from 'react-native-webview';
 import { useState } from "react";
 import { useSelector } from "react-redux";
@@ -8,44 +8,12 @@ const CrossfitDayScreen = ({ route }) => {
     const [selectedWorkout, setSelectedWorkout] = useState({});
     const crossfitWorkouts = useSelector((state) => state.crossfitWorkouts)  
     const data = crossfitWorkouts.crossfitWorkoutsArray.filter((crossfitWorkouts) => crossfitWorkouts.day === route.params.item.name)
-
-    // const renderModal = (value) => {
-    //     console.log(value.link)
-    //     return (
-    //         <Modal
-    //             animationType='slide'
-    //             transparent={false}
-    //             visible={showModal}
-    //             onRequestClose={() => setShowModal(!showModal)}
-    //         >
-    //             <View style={{ 
-    //                 flex: 1,
-    //                 backgroundColor: 'orange',
-    //                 borderWidth: 1,
-    //                 borderColor: 'orange',
-    //                 marginTop: 22 }}
-    //             >
-    //                 <Text>{value.link}</Text>
-    //                 <WebView
-    //                     javaScriptEnabled={true}
-    //                     style={{flex:1, borderColor:'red', borderWidth:1, height:400, width:400}}
-    //                     source={{
-    //                         uri: 'https://www.youtube.com/embed/RJa4kG1N3d0'
-    //                     }}
-    //                 />
-    //                 <TouchableOpacity onPress={() => setShowModal(!showModal)}>
-    //                     <Text>Hide Modal</Text>
-    //                 </TouchableOpacity>
-    //             </View>
-    //         </Modal>
-    //     )
-    // }
     
     const renderWorkouts = ({ item }) => { 
 
         return(
-            <View>
-                <Text>{item.type}</Text>
+            <View style={styles.workoutView}>
+                <Text style={styles.workoutType}>{item.type}</Text>
                 { item.workout.map((value, index) => {
                     return (
                         <Text 
@@ -73,21 +41,36 @@ const CrossfitDayScreen = ({ route }) => {
                         borderColor: 'orange',
                         marginTop: 22 }}
                     >
-                        <Text>{selectedWorkout.link}</Text>
                         <WebView
                             javaScriptEnabled={true}
                             style={{flex:1, borderColor:'red', borderWidth:1, height:400, width:400}}
                             source={{
-                                uri: 'https://www.youtube.com/embed/RJa4kG1N3d0'
+                                uri: selectedWorkout.link
                             }}
                         />
-                        <TouchableOpacity onPress={() => setShowModal(!showModal)}>
-                            <Text>Hide Modal</Text>
-                        </TouchableOpacity>
+                    </View>
+                    <View>
+                        <Button
+                            onPress={() => setShowModal(!showModal)}
+                            color='#808080'
+                            title='Exit'
+                        />
                     </View>
                 </Modal>
             </View>
         )    
+    }
+
+    const ItemDivider = () => {
+        return (
+          <View
+            style={{
+              height: 4,
+              width: "100%",
+              backgroundColor: "#607D8B",
+            }}
+          />
+        );
     }
 
     return (
@@ -95,7 +78,8 @@ const CrossfitDayScreen = ({ route }) => {
             <FlatList
                data={data}
                renderItem={renderWorkouts}
-               keyExtractor={(item) => item.id.toString()} 
+               keyExtractor={(item) => item.id.toString()}
+               ItemSeparatorComponent={ItemDivider} 
             />
         </View>
     )
@@ -104,7 +88,17 @@ const CrossfitDayScreen = ({ route }) => {
 const styles = StyleSheet.create({
     workoutText: {
         textAlign: 'center',
-        fontSize: 15
+        fontSize: 20,
+        margin: 10
+    },
+    workoutType: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 50,
+        marginBottom: 15
+    },
+    workoutView: {
+        marginBottom: 25
     }
 })
 
